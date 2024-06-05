980948cd6399ec307010bcd3c50a8d1249cce085
Silvia Lozano <sylvilozano@gmail.com>
2024-06-05 16:17:50 +0200

    feature: added borders select and service

--- src/app/countries/selector-page/selector-page.component.ts
+++ src/app/countries/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../services/countries.service';
 import { Region, smallCountry } from '../interfaces/country.interface';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -16,16 +16,15 @@ export class SelectorPageComponent implements OnInit {
     borders: ['', Validators.required],
   });
   countires = <smallCountry[]>[];
+  borders = <smallCountry[]>[];
   constructor(
     private fb: FormBuilder,
     private countriesService: CountriesService
   ) {}
 
   ngOnInit(): void {
-    this.myForm.get('region')?.valueChanges.subscribe((region) => {
-      this.onRegionChange();
-      console.log({ region });
-    });
+    this.onRegionChange();
+    this.onCountryChange();
   }
 
   get regions(): Region[] {
@@ -36,12 +35,38 @@ export class SelectorPageComponent implements OnInit {
     this.myForm
       .get('region')!
       .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
         switchMap((region) => this.countriesService.onRegionChange(region))
       )
       .subscribe(
         (countries) => {
           console.log('countries in onRegionChange ', { countries });
           this.countires = countries;
+        },
+        (error) => {
+          console.error('Error in subscribe', error);
+        }
+      );
+  }
+
+  onCountryChange() {
+    //const borders = this.myForm.get('borders')?.value;
+    this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('borders')!.setValue('')),
+        filter((value) => value.length > 0),
+        switchMap((alfphaCode) =>
+          this.countriesService.getCountryByAlphaCode(alfphaCode)
+        ),
+        switchMap((country) =>
+          this.countriesService.getCountryBordersByCode(country.borders)
+        )
+      )
+      .subscribe(
+        (country) => {
+          console.log('borders in onRegionChange ', { country });
+          this.borders = country;
         },
         (error) => {
           console.error('Error in subscribe', error);
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Country, Region, smallCountry } from '../interfaces/country.interface';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, map, of, tap } from 'rxjs';
+import {
+  Observable,
+  catchError,
+  combineLatest,
+  filter,
+  map,
+  of,
+  tap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -45,5 +53,37 @@ export class CountriesService {
         return of([]);
       })
     );
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<smallCountry> {
+    const url = `${this.baseUrl}alpha/${alphaCode}`;
+    return this.http.get<Country>(url).pipe(
+      map((country: Country) => ({
+        name: country?.name?.common || 'no hay name',
+        cca3: country?.cca3 || 'no hay cca3',
+        borders: country?.borders || ['no hay borders'],
+      })),
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error occurred:', error);
+        // Devuelve un objeto smallCountry con valores por defecto en caso de error
+        return of({
+          name: 'Error',
+          cca3: 'Error',
+          borders: ['Error'],
+        });
+      })
+    );
+  }
+
+  getCountryBordersByCode(borders: string[]): Observable<smallCountry[]> {
+    if (!borders) {
+      return of([]);
+    }
+    const peticiones: Observable<smallCountry>[] = [];
+    borders.forEach((codigo) => {
+      const peticion = this.getCountryByAlphaCode(codigo);
+      peticiones.push(peticion);
+    });
+    return combineLatest(peticiones);
   }
 }
